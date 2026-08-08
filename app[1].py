76c79debf74338bbd0f3d0e2732d75fb0b165cbb
MUHAMMAD HAMMAD <mhammadmnaeem29@gmail.com>
2026-08-08 12:42:19 +0500

    app.py

--- app[1].py
+++ app[1].py
@@ -28,6 +28,7 @@ Run with:  streamlit run app.py
 ================================================================================
 """
 
+
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
